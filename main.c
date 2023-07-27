c2be073db1ab66f11ade459671ed79b73611dec3
EMarvelM <marvelousegbe19@gmail.com>
2023-07-27 18:36:03 +0100

    allocate memory for nodes

--- main.c
+++ main.c
@@ -4,10 +4,30 @@
 /**
 * main - entry point
 *
+* 65 days in alx
+*
 * Return: unkown.
 */
 
-struct node {
-  int data;
+/* create a datatype */
+struct node 
+{
+  int value;
+  char *p;
   struct node *next;
+};
+
+int main(void)
+{
+  /* of the linked list */
+  /* struct node *head; */
+
+  /* create a new list called newnode */
+  struct node *newNode1 = (struct node *)malloc(sizeof(struct node));
+  struct node *newNode2 = (struct node *)malloc(sizeof(struct node));
+  struct node *newNode3 = (struct node *)malloc(sizeof(struct node));
+  
+  /* head = newNode; */
+
+
 }
