c091f2c459956c6ff38eb568be017f82375916e9
EMarvelM <marvelousegbe19@gmail.com>
2023-07-30 07:27:43 +0100

    perform NOR operation

--- bit_manipulation/bit.c
+++ bit_manipulation/bit.c
@@ -12,5 +12,9 @@ int main(void)
     // OR
     printf("OR: %d\n", x | y); // Result: 0000000000001110 (14 in decimal)
 
+    // NOR
+    //How it works
+    printf("NOR: %d\n", z ^ x); // Result: 0000000000001010 (10 in decimal)
+
     return 0;
 }
