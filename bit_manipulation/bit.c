5113a153260bd374cb19286172e7f7c4be5edb98
EMarvelM <marvelousegbe19@gmail.com>
2023-07-30 07:13:00 +0100

    format code

--- bit_manipulation/bit.c
+++ bit_manipulation/bit.c
@@ -2,13 +2,15 @@
 
 int main(void)
 {
-    int z = 12; //0000000000000110
-    int y = 8;  //0000000000001000
-    int x = 6;
+    int z = 12; // 0000000000001100 in binary
+    int y = 8;  // 0000000000001000 in binary
+    int x = 6;  // 0000000000000110 in binary
 
-    //AND
-    printf("AND: %d\n", z & y); //0000000000001000
+    // AND
+    printf("AND: %d\n", z & y); // Result: 0000000000001000 (8 in decimal)
 
-    //OR
-    printf("OR: %d\n", x | y); //0000000000001110
+    // OR
+    printf("OR: %d\n", x | y); // Result: 0000000000001110 (14 in decimal)
+
+    return 0;
 }
