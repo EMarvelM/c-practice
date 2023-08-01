41584efd1149945804766eba3559cd84753e337f
EMarvelM <marvelousegbe19@gmail.com>
2023-08-01 16:35:01 +0100

    defined function

---
+++ singly_linked_lists/print_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_listint - traverse linked list printing each data
+ * @h: points to the head
+ * 
+ * Return: 0 on success
+*/
+
+size_t print_listint(const listint_t *h)
+{
+    
+}
