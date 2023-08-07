dbf6e560815e0c8f1e508b71ab1141c5c0628488
EMarvelM <marvelousegbe19@gmail.com>
2023-08-07 15:28:56 +0100

    handling tokenization

--- simple_sh/_strtok.c
+++ simple_sh/_strtok.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+int main()
 {
-    char *buffer = NULL;
-    long unsigned int n = 0;
-    
-    //loop printing "$ " and displaying user input
-    while(1)
-    {
-        printf("$ ");
-        getline(&buffer, &n, stdin);
+        char *buffer;
+        long unsigned int n = 0;
+        char *del = " \n";
 
-        printf("%s", buffer);
-    }
+        while(1)
+        {
+                /* Prompting the user in a loop*/
+                printf("$ ");
+                getline(&buffer, &n, stdin); /* stores each line using getline to var: buffer */
+
+                char *token = strtok(buffer, del); /* makes buffer store only first argument v[0] */
+
+                printf("buffer: %s\n", buffer);
+
+                /* Stores the argument v[1,2, ...] of the user input */
+                while (token != NULL)
+                {
+                        token = strtok(NULL, del);
+
+                        /* Stop printing the NULL token*/
+                        if (token != NULL)
+                        printf("token: %s\n", token);
+                }
+        }
 }
