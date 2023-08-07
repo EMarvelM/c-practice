e920d7fdf0676e347647d2322043e7506d20b968
EMarvelM <marvelousegbe19@gmail.com>
2023-08-07 16:48:38 +0100

    parsing each token to a allocated memory as arguments

--- simple_sh/_strtok.c
+++ simple_sh/_strtok.c
@@ -6,6 +6,7 @@ int main()
         char *buffer;
         long unsigned int n = 0;
         char *del = " \n";
+        char *argv;
 
         while(1)
         {
@@ -14,17 +15,29 @@ int main()
                 getline(&buffer, &n, stdin); /* stores each line using getline to var: buffer */
 
                 char *token = strtok(buffer, del); /* makes buffer store only first argument v[0] */
+                argv[0] = buffer;
 
                 printf("buffer: %s\n", buffer);
 
                 /* Stores the argument v[1,2, ...] of the user input */
+                int i = 0;
                 while (token != NULL)
                 {
+                    int count = 0;
+                    i++;
                         token = strtok(NULL, del);
-
+                            while (token)
+                            {
+                                count++;
+                            }
+                            
                         /* Stop printing the NULL token*/
                         if (token != NULL)
-                        printf("token: %s\n", token);
+                        {
+                            argv[i] = malloc(sizeof(char) * count);
+                            strcpy(argv[i], token);
+                            printf("token: %s\n", token);
+                        }
                 }
         }
 }
