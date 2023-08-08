fc9fea2e9f46dbbe70f336805397ace7063b9c45
EMarvelM <marvelousegbe19@gmail.com>
2023-08-08 12:50:29 +0100

    allocating memory for each string in the token

--- simple_sh/_strtok.c
+++ simple_sh/_strtok.c
@@ -6,16 +6,24 @@ int main()
         char *buffer = NULL;
         size_t n = 0;
         char *del = " \n";
-        char *argv;
+        char **argv = NULL;
 
         while(1)
         {
+            int argc = 0;
+            
                 /* Prompting the user in a loop*/
                 printf("$ ");
                 getline(&buffer, &n, stdin); /* stores each line using getline to var: buffer */
 
+            while(buffer)
+            {
+                argc++;
+            }
+            **argv = (char **)malloc(sizeof(char *) * (argc +1));
+
                 char *token = strtok(buffer, del); /* makes buffer store only first argument v[0] */
-                argv[0] = buffer;
+                //argv[0] = buffer;  /* Not accepted */
 
                 printf("buffer: %s\n", buffer);
 
@@ -26,6 +34,7 @@ int main()
                     int count = 0;
                     i++;
                         token = strtok(NULL, del);
+                        *argv = (char *)malloc(strlen(token)+1);
                             while (token)
                             {
                                 count++;
@@ -35,7 +44,7 @@ int main()
                         if (token != NULL)
                         {
                             /* parsing each token to a allocated memory as arguments */
-                            argv[i] = (char *)malloc(sizeof(char) * count);
+                            /* argv[i] = (char *)malloc(sizeof(char) * count); */
                             strcpy(argv[i], token);
                             printf("token: %s\n", token);
                         }
