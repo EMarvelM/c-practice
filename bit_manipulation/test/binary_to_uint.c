3d035aceed37ac79cc9638ffd5daf640ad9dbfec
emarvelm <marvelousegbe19@gmail.com>
2023-08-30 12:54:25 +0000

    trying to convert binary to uint

--- bit_manipulation/test/binary_to_uint.c
+++ bit_manipulation/test/binary_to_uint.c
@@ -7,28 +7,34 @@
 */
 
 unsigned int binary_to_uint(const char *b)
-{
+{	
+	unsigned int result = 0;
+	int power = 0;
 	int track = 0;
 
 	while(b[track] != '\0')
 	{
 		track++;
 	}
+	track--; /* returned the tracker from the null char */
 
-	while(b[track] >= 0)
+	while(track >= 0)
 	{
-		if (b[track] == 1)
+		if (b[track] == '1')
 		{
-			printf("it is %d", b[track]);
+			1 << power;
+			power++;
 		}
-		else if (b[track] == 0)
+		else if (b[track] == '0')
 		{
-			printf("actually = %d", b[track]);
+			power++;
 		}
 		else{
-			printf("not a digit");
+			return (0);
 		}
 		track--;
+
+		return (result);
 	}
 	/* printf("the value of track = %d\n", track); */
 }
