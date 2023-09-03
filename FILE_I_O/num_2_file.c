35ee89a9f7787ec4bc41fa56c2303e7cc57267a7
EMarvelM <marvelousegbe19@gmail.com>
2023-09-03 09:27:13 +0000

    program to put entered digits to a file(-1 terminate)

---
+++ FILE_I_O/num_2_file.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * 
+ * Returns: 0 on success.
+*/
+
+int main(void)
+{
+	int nums;
+	FILE *fp = NULL;
+
+	fp = fopen("number.txt", "w+");
+	if (!fp)
+	{
+		printf("error opening file");
+		exit(1);
+	}
+
+	/* processing */
+
+	do {
+		printf("Enter a #(-1 to quit): ");
+		scanf("%d", &nums);
+		fprintf(fp ,"%d\n", nums);
+	} while (nums != -1);
+
+
+
+	fclose(fp);
+	return (0);
+}
