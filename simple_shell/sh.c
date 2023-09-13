3d61054524fe64f78c9e24042dac1e8b80ccb39e
EMarvelM <marvelousegbe19@gmail.com>
2023-09-13 17:54:53 +0100

    removed cmd_mode from main file

--- simple_shell/sh.c
+++ simple_shell/sh.c
@@ -13,22 +13,4 @@ int main(void)
 	return (0);
 }
 
-void cmd_mode(void)
-{
-	int mode;
-
-	if ((mode = isatty(STDIN_FILENO)) == 0)
-	{
-		if (errno == EBADF)
-		{
-			perror("\nNo Valid fd!");
-			exit(-1);
-		}
-		else if(errno == ENOTTY || errno == EINVAL)
-		{
-			perror("\nNo terminal!");
-			exit(0);
-		}
-	}
-}
 
