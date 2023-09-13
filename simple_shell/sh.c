0eaba1c92136da80f04114be61c127efc8c12930
EMarvelM <marvelousegbe19@gmail.com>
2023-09-13 17:42:01 +0100

    check for isatty

--- simple_shell/sh.c
+++ simple_shell/sh.c
@@ -2,11 +2,34 @@
 
 int main(void)
 {
-	 while(1) {
+	while(1) 
+	{
 		sh_prompt("Cisfun# ");
 		read_cmd();
+
+		/* isatty check here */
+		cmd_mode();
 	}
 	return (0);
+}
+
+void cmd_mode(void)
+{
+	int mode;
+
+	if ((mode = isatty(STDIN_FILENO)) == 0)
+	{
+		if (errno == EBADF)
+		{
+			perror("\nNo Valid fd!");
+			exit(-1);
+		}
+		else if(errno == ENOTTY || errno == EINVAL)
+		{
+			perror("\nNo terminal!");
+			exit(0);
+		}
+	}
 }
 
 void read_cmd(void)
@@ -16,17 +39,18 @@ void read_cmd(void)
 	size_t n = 0;
 	size_t char_n;
 
-	if ((char_n = getline(&buffer, &n, stdout)) == ((size_t)-1))
-	{
+	char_n = getline(&buffer, &n, stdin);
 
+	if (char_n == ((size_t)-1))
+	{
 		if (char_n == (size_t)EOF && errno == 0)
 		{
-			perror("EOF Reached!");
+			perror("\nEOF Reached!");
 			exit(1);
 		}
 		else if (errno == EINVAL || errno == ENOMEM)
 		{
-			perror("getline error!");
+			perror("\ngetline error!");
 			exit(1);
 		}
 	}
