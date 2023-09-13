420bdb35fe55d4cb8c458ba14acef9bda1ef87cc
EMarvelM <marvelousegbe19@gmail.com>
2023-09-13 17:52:41 +0100

    removed read_cmd from main file

--- simple_shell/sh.c
+++ simple_shell/sh.c
@@ -32,28 +32,3 @@ void cmd_mode(void)
 	}
 }
 
-void read_cmd(void)
-{
-		/* getline */
-	char *buffer = NULL;
-	size_t n = 0;
-	size_t char_n;
-
-	char_n = getline(&buffer, &n, stdin);
-
-	if (char_n == ((size_t)-1))
-	{
-		if (char_n == (size_t)EOF && errno == 0)
-		{
-			perror("\nEOF Reached!");
-			exit(1);
-		}
-		else if (errno == EINVAL || errno == ENOMEM)
-		{
-			perror("\ngetline error!");
-			exit(1);
-		}
-	}
-
-	free(buffer);
-}
