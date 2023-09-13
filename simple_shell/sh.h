2f87d3a636a1f0556dfa7c4af3773c4f52e2744b
EMarvelM <marvelousegbe19@gmail.com>
2023-09-13 17:22:38 +0100

    added more reasons

--- simple_shell/sh.h
+++ simple_shell/sh.h
@@ -2,7 +2,7 @@
 #define SH_H
 
 #include <stdio.h> /* while testing (printf, getline, perror)*/
-#include <unistd.h> /* write */
+#include <unistd.h> /* write, isatty*/
 #include <stdlib.h> /* exit */
 #include <errno.h> /* ENIVAL, */
 
