9144b60941e3ad237c038d1dc91f98a99c493c26
EMarvelM <marvelousegbe19@gmail.com>
2023-09-13 18:05:07 +0100

    betty reject use of assignement in if

--- simple_shell/cmd_mode.c
+++ simple_shell/cmd_mode.c
@@ -1,17 +1,24 @@
 #include "sh.h"
 
+/**
+ * cmd_mode - checks for interactive mode (terminal).
+ *
+ * Return: void.
+*/
 void cmd_mode(void)
 {
 	int mode;
 
-	if ((mode = isatty(STDIN_FILENO)) == 0)
+	mode = isatty(STDIN_FILENO);
+
+	if (mode == 0)
 	{
 		if (errno == EBADF)
 		{
 			perror("\nNo Valid fd!");
 			exit(-1);
 		}
-		else if(errno == ENOTTY || errno == EINVAL)
+		else if (errno == ENOTTY || errno == EINVAL)
 		{
 			perror("\nNo terminal!");
 			exit(0);
