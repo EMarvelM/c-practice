3e07759ee565c4ced9252fb05a065e9edf959993
EMarvelM <marvelousegbe19@gmail.com>
2023-09-13 17:14:50 +0100

    standard libraries

--- simple_shell/sh.h
+++ simple_shell/sh.h
@@ -1,6 +1,9 @@
 #ifndef SH_H
 #define SH_H
 
-
+#include <stdio.h> /* while testing (printf, getline, perror)*/
+#include <unistd.h> /* write */
+#include <stdlib.h> /* exit */
+#include <errno.h> /* ENIVAL, */
 
 #endif /* SH_H */
