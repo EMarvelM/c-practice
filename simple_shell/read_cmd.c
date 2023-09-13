ca2c1b78768a02125c46af3f2d7b7e660e51f626
EMarvelM <marvelousegbe19@gmail.com>
2023-09-13 17:52:09 +0100

    moved read_cmd function to new file

---
+++ simple_shell/read_cmd.c
@@ -0,0 +1,27 @@
+#include "sh.h"
+
+void read_cmd(void)
+{
+		/* getline */
+	char *buffer = NULL;
+	size_t n = 0;
+	size_t char_n;
+
+	char_n = getline(&buffer, &n, stdin);
+
+	if (char_n == ((size_t)-1))
+	{
+		if (char_n == (size_t)EOF && errno == 0)
+		{
+			perror("\nEOF Reached!");
+			exit(1);
+		}
+		else if (errno == EINVAL || errno == ENOMEM)
+		{
+			perror("\ngetline error!");
+			exit(1);
+		}
+	}
+
+	free(buffer);
+}
