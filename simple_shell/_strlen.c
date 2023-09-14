ec30614044341759108a8d09dde188feaa0fba27
EMarvelM <marvelousegbe19@gmail.com>
2023-09-14 13:52:55 +0100

    following betty's rules

--- simple_shell/_strlen.c
+++ simple_shell/_strlen.c
@@ -2,20 +2,22 @@
 
 /**
  * _strlen - calcucate the lenth of a string
- * 
+ * @_string: string to count
+ *
+ * Return: total character count excluding '\0'
 */
 
-size_t _strlen(char *_string)
+size_t _strlen(const char *_string)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while(_string[count])
-    {
-        if (_string[count])
-            count++;
-    }
-    /*testing works :)*/
-    printf("_strlen == %ld", count);
+	while (_string[count])
+	{
+		if (_string[count])
+			count++;
+	}
+	/*testing works :)*/
+	/*printf("_strlen == %ld", count);*/
 
-    return (count);
+	return (count);
 }
