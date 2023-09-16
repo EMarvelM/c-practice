d23c04aa6860111cadb982f3178c36db5f68bef2
EMarvelM <marvelousegbe19@gmail.com>
2023-09-16 17:49:02 +0100

    handling signal for Ctrl + D

--- simple_shell/read_cmd.c
+++ simple_shell/read_cmd.c
@@ -12,8 +12,25 @@ char **read_cmd(void)
 	size_t n = 0;
 	size_t char_n;
 	char **argv = NULL;
+	uintptr_t sign;
+	static int sign_c = 0;
+
 
 	char_n = getline(&buffer, &n, stdin);
+
+	/*make sure signal is called once throughout the program*/
+	sign_c++;
+	if (sign_c == 0)
+	sign = (uintptr_t) signal(SIGINT, sigintHandler);
+
+
+	/*free if Ctrl + C*/
+	if (sign == SIGINT)
+	{
+		free (buffer);
+		exit(0);
+	}
+
 
 	if (char_n == ((size_t)-1))
 	{
@@ -35,8 +52,11 @@ char **read_cmd(void)
 	/* tokenization comes here!!! */
 	argv = _tokenise(buffer, " \n");
 
-	/* testing */
-	/* printf("argp == %s\n", buffer); */
-
 	return (argv);
+}
+
+void sigintHandler(int signum)
+{
+	signum++;
+	signum--;
 }
