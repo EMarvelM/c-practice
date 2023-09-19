41c80092b3964cb3f27d0633776099d0abd98102
EMarvelM <marvelousegbe19@gmail.com>
2023-09-19 16:25:27 +0100

    breaking the path collected from the main function (tokenize)

--- simple_shell/sh.c
+++ simple_shell/sh.c
@@ -28,3 +28,77 @@ int main(int ac, char **agv, char **envp)
 
 	return (0);
 }
+
+
+char **pather(char **envp)
+{
+	int total_p = 1;
+	int j, i;
+	char *temp;
+	int tok_len;
+	char *test = NULL, **arr;
+
+	j = 0;
+	while (envp[j])
+	{	
+			
+		if ((strncmp(envp[j], "PATH", 3) == 0))
+		{
+			temp = strdup(envp[j]);
+			if (!temp)
+			{
+				perror("strdup failed");
+				exit(0);
+			}
+
+			for (i = 0; temp[i] != '\0'; i++)
+			{
+				if (temp[i] == ':')
+				{
+					total_p++;
+				}
+			}
+			arr = (char **)malloc(sizeof(char *) * (total_p + 1)); /*+1 for null*/
+			if (arr == NULL)
+			{
+				perror("malloc failed");
+			}
+
+			for (i = 0; i < (total_p); i++)
+			{
+				if (i == 0)
+				{
+					test = strtok(temp, ":\n");
+					if (test == NULL)
+						return(arr);
+
+					arr[i] = (char *)malloc(sizeof(char) * (strlen(test) + 1));
+					printf("%s\n", arr[i]);
+					
+					if (arr[i] == NULL)
+					{
+						perror("malloc failed");
+						exit (1);
+					}
+				}
+				else
+				{
+					test = strtok(NULL, ":\n");
+					arr[i] = (char *)malloc(sizeof(char) * (strlen(test) + 1));
+					if (arr[i] == NULL)
+					{
+						perror("malloc failed");
+						exit (1);
+					}
+					strcpy(arr[i], test);
+					printf("%s\n", test);
+
+				}
+			}
+			arr[i] = NULL;
+		}
+		j++;
+	}
+
+	return (arr);
+}
