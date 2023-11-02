83edcd185cb71c78cde43e9b37b4f0540226c355
EMarvelM <marvelousegbe19@gmail.com>
2023-11-02 16:44:17 +0100

    feat: 2-key_index

--- hash/2-key_index.c
+++ hash/2-key_index.c
@@ -1,6 +1,15 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - determine the index of a key
+ * @key: the key
+ * @size: size of the array in the hash table
+ *
+ * Return: the index
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int idx = hash_djb2()
+    unsigned long int idx = hash_djb2(key) % size;
+
+    return (idx);
 }
