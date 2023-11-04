d1b64f519455959dd260c95739bd6980bb1b8598
EMarvelM <marvelousegbe19@gmail.com>
2023-11-04 13:52:48 +0100

    feat: 3-hash_table_set

--- hash/3-hash_table_set.c
+++ hash/3-hash_table_set.c
@@ -7,6 +7,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     unsigned long int hash = hash_djb2((const unsigned char*)key);
     unsigned long int idx = hash % ht->size;
 
+
     if (key == NULL)
     {
         return (0);
@@ -23,6 +24,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
     if (ht->array[idx] == NULL)
     {
+        hash_node_t *newPtr = malloc(sizeof(hash_node_t));
+        ht->array[idx] = newPtr;
+        printf(" - >>> %lu\n", idx);
         ht->array[idx]->key = _key;
         ht->array[idx]->value = val;
         ht->array[idx]->next = NULL;
