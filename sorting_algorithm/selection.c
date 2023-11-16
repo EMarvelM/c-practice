5c0751dbbd61fb03614cfff9fda7992f110d30fd
EMarvelM <marvelousegbe19@gmail.com>
2023-11-16 14:57:39 +0100

    fix: selection sorting now sort as expected

--- sorting_algorithm/selection.c
+++ sorting_algorithm/selection.c
@@ -20,7 +20,7 @@ int main(void)
 	/* Call the bubbleSort function */
 
 	/* Display the sorted array */
-	printf("\nSorted array:");
+	printf("\nUnSorted array:");
 	for (int j = 0; j < n; j++)
 	{
 		printf(" %d", arr[j]);
@@ -39,20 +39,18 @@ void selectionSort(int arr[], int n)
 	for (i = 0; i < n; i++)
 	{
 		/*while looping through array, */
-		for (j = 0; j < n; j++)
+		for (j = 0 + i; j < n; j++)
 		{
 			/*find the smallest element in the array*/
 			if (arr[j] < arr[i])
 			{
-				min_idx = j;
 				/*swap the smallest element with the first element*/
-				j = arr[min_idx];
-				arr[min_idx] = arr[i];
-				arr[i] = j;
+				min_idx = arr[i];
+				arr[i] = arr[j];
+				arr[j] = min_idx;
 			}
 
 		}
-
 
 		printf("Sorted array:");
 		for (int j = 0; j < n; j++)
