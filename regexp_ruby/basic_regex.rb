bb7b8d6b640b04820d8d5d89799a50e5ee7c9132
EMarvelM <marvelousegbe19@gmail.com>
2023-11-29 13:46:58 +0100

    trinary and conditional

--- regexp_ruby/basic_regex.rb
+++ regexp_ruby/basic_regex.rb
@@ -3,3 +3,10 @@ string = "The quick 12 brown foxes jump over the 10 lazy dogs"
 
 p string =~ /quick/
 
+p string =~ /z/ ? "Valid string" : "Invalid"
+
+if string =~ /z/
+    puts "valid"
+else
+    puts "Invalid"
+end
