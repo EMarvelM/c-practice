f8d926a043584aa80778f53f4334d6476415a900
EMarvelM <marvelousegbe19@gmail.com>
2024-02-09 09:57:43 +0100

    Documentation: clear explanation

--- programming_skills/1-find_the_difference.c
+++ programming_skills/1-find_the_difference.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 char findTheDifference(char* s, char* t) {
+	/* Return null character if the string is NULL */
 	if (!s && !t) return ('\0');
 	else if (!s) return ('\0');
 	else if (!t) return ('\0');
 
-	char temp;
+	/* initialize i and j for the loop */
 	int i = 0, j = 0;
 
 	for (i = 0; t[i] != '\0'; i++)
 	{
+		/* for every letter in the longer string `t` */
 		for(j = 0; s[j] != '\0'; j++)
 		{
+			/* for every letter in the shortest string `s` */
 			if (t[i] == s[j]) break;
+				/*if they match, stop searching the particular t index*/
 		}
-		printf("Value i[%c] = %d\t", t[i], i);
-		printf("Value j[%c] = %d\n", s[j], j);
+		/*if particular index in t wasn't found in s then we got the new letter*/
 		if (t[i] != s[j]) break;
 	}
-	return (t[i]);
-
-	/* return (t[strlen(s)]); */
+	return (t[i]); /* return the letter that was added to `t`*/
 }
