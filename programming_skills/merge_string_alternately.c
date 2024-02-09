3a57443860e235cef3c67716df089e00e7bec811
EMarvelM <marvelousegbe19@gmail.com>
2024-02-09 03:57:28 +0100

    Used tab size 4 for indentation and return if any word is NULL

--- programming_skills/merge_string_alternately.c
+++ programming_skills/merge_string_alternately.c
@@ -1,44 +1,57 @@
 #include "main.h"
 
 char * mergeAlternately(char * word1, char * word2){
-  int len;
+	if (!word1 && !word2)
+		return (NULL);
+	else if (!word1)
+		return (word2);
+	else if (!word2)
+		return (word1);
 
-  len = strlen(word1);
-  len += strlen(word2);
+	int len;
 
-  char *merged = (char *)malloc(sizeof(char) * len + 1);
+	len = strlen(word1);
+	len += strlen(word2);
 
-  int i, j = 0, k = 0;
+	char *merged = (char *)malloc(sizeof(char) * (len + 1));
+	if (merged == NULL)
+	{
+		perror("Malloc failed!");
+		exit(-1);
+	}
 
-  for(i = 0; i < len ; i++)
-    {
-      if (word1[k] && word2[j])
-      {
-        if ((i % 2) == 0)
-          {
-              merged[i] = word1[k];
-              k++;
-          }
-        else if ((i % 2) != 0)
-          {
-              merged[i] = word2[j];
-              j++;
-          }
-        }
-        else
-        {
-          if (word1[k])
-          {
-            merged[i] = word1[k];
-            k++;
-          }
-          else if (word2[j])
-          {
-            merged[i] = word2[j];
-            j++;
-          }
-        }
-    }
+	int i, j = 0, k = 0;
 
-  return (merged);
+	for(i = 0; i < len ; i++)
+		{
+			if (word1[k] && word2[j])
+			{
+				if ((i % 2) == 0)
+					{
+							merged[i] = word1[k];
+							k++;
+					}
+				else if ((i % 2) != 0)
+					{
+							merged[i] = word2[j];
+							j++;
+					}
+				}
+				else
+				{
+					if (word1[k])
+					{
+						merged[i] = word1[k];
+						k++;
+					}
+					else if (word2[j])
+					{
+						merged[i] = word2[j];
+						j++;
+					}
+				}
+		}
+		merged[i] = '\0';
+
+	return (merged);
 }
