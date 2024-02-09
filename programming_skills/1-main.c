df3fe872b47f4158d44b65383f2814d6ea90cc9f
EMarvelM <marvelousegbe19@gmail.com>
2024-02-09 09:58:07 +0100

    test: to find the difference in letter

---
+++ programming_skills/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(void)
+{
+    char *s = "";
+    char *t = "y";
+    char result = findTheDifference(s, t);
+
+    printf("Result == %c", result);
+
+    return (0);
+}
