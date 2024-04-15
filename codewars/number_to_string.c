f4a33310c12f96815a7d027140efd47be1071fbc
emarvelm <marvelousegbe19@gmail.com>
2024-04-15 02:30:19 +0100

    conversion not complete

--- codewars/number_to_string.c
+++ codewars/number_to_string.c
@@ -14,8 +14,8 @@ int main(void)
 	char *str;
 	str = number_to_string(num);
 	
-	//if (str)
-		//printf("string == %s", str);
+	if (str)
+		printf("string == %s\n", str);
 }
 
 
@@ -23,24 +23,29 @@ char *number_to_string(int number) {
 
     //  <----  hajime!
 	int i, rem, s = 1;
-	int num = malloc(sizeof(int) * s);
+	int *num = malloc(sizeof(int) * s);
 	int siz = 0;
+	char *c = NULL;
 
 	for (i = 0; number > 0; i++)
 	{
 		rem = number % 10; /* get last digit */
 		number = number / 10;
 		num[i] = rem;
+		/* verbose  printf("s = %d\n", s); */
 		num = realloc(num, sizeof(int) * ++s);
 	}
-	
-	c = malloc(sizeof(char) * s + 1);
+	c = malloc(sizeof(char) * s);
+	s--;
 
-	siz = sizeof(num) / sizeof(int);
-	for (i = 0; i < siz; i++)
+	for (i = 0; i < s; i++) { printf("%d", num[i]); }
+	printf("\n");
+
+	for (i = 0; i < s; i++)
 	{
-		c[i] siz - i;
+		c[i] = num[s - i];
 	}
+	c[i] = '\0';
 	return (c);
 }
 
